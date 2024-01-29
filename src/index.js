3d9fbecf5a25f1828eac4bf75a0cd74465120e7b
JonatanDrysen <jojodrysen@gmail.com>
2024-01-29 10:51:09 +0100

    add unfinished renderTodoList fn

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function renderTodoList() {
+    const container = document.querySelector(".todoListContainer")
+    const todoList = document.createElement("ul")
+    const todo = document.createElement("li")
+
+    const todoContainer = document.createElement("div")
+}
